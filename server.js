c8c8ae0af02ebcc41b5eb5f66b2f7b25833af2e4
michael <michaelgdasaro@gmail.com>
2019-10-19 14:29:44 -0400

    Testing that I won't upload api key

--- server.js
+++ server.js
@@ -3,5 +3,11 @@ const client = require('socket.io').listen(4000).sockets;
 client.on('connection', function(socket){
     socket.on("upload", function(data){
     //data.file
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',["./mapper.py",
+                            req.query.data.file] );
+    process.stdout.on('data', function(data) {
+      console.log(data);
+    });
   });
 });
