9aeea40d8be8c2e6d74949a4125216cc13d2bae5
michael <michaelgdasaro@gmail.com>
2019-10-19 13:31:27 -0400

    File upload passes

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-const client = require('socket.io').listen(4000).sockets;
 const http = require("http");
+const client = require('socket.io').listen(4000).sockets;
+client.on('connection', function(socket){
+    socket.on("upload", function(data){
+    //data.file
+  });
+});
